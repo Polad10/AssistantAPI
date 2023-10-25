6ffe64e78621045d14aba7c08bf25e31702b23a0
Polad <samadzada.polad@gmail.com>
2023-10-25 14:23:04 +0200

    create treatment + validator

--- routes/treatmentRoutes.ts
+++ routes/treatmentRoutes.ts
@@ -1,5 +1,7 @@
-import express from 'express';
-import { PrismaClient, Prisma } from '@prisma/client'
+import express, {Request, Response} from 'express';
+import { PrismaClient } from '@prisma/client'
+import httpStatusCodes from '../constants/httpStatusCodes.js';
+import treatmentPostValidator from './validators/treatmentValidators.js';
 
 const router = express.Router()
 const prisma = new PrismaClient()
@@ -8,6 +10,19 @@ router.get('/', async (req, res) => {
   const treatments = await prisma.treatment.findMany()
   
   res.json(treatments)
+})
+
+router.post('/', treatmentPostValidator, async (req: Request, res: Response) => {
+  try {
+    const treatment = await prisma.treatment.create({
+      data: req.body
+    })
+
+    res.status(httpStatusCodes.created).json(treatment)
+  }
+  catch(ex) {
+    res.sendStatus(httpStatusCodes.internalServerError)
+  }  
 })
 
 export default router
---
+++ routes/validators/treatmentValidators.ts
@@ -0,0 +1,11 @@
+import { body } from "express-validator"
+import validationErrorCheck from "./validationErrorCheck.js"
+
+const treatmentPostValidator = [
+  body('start_date').notEmpty().isISO8601().toDate(),
+  body('title').notEmpty(),
+  body('patient_id').notEmpty().isInt(),
+  validationErrorCheck
+]
+
+export default treatmentPostValidator
